eb1067a01722e3a6077b89a7755fbba299959247
Danilo Gonçalves Pinheiro <124277720+DaniloPin@users.noreply.github.com>
2025-06-23 19:50:58 -0300

    Add files via upload

---
+++ HistoricoMedico.cs
@@ -0,0 +1,12 @@
+﻿internal class HistoricoMedico {
+    public string CodigoProntuario {  get; }
+
+    public HistoricoMedico(string codigoProntuario) {
+        CodigoProntuario = codigoProntuario;
+    }
+
+    public void ExibirCodigo() {
+
+        Console.WriteLine($"Código do prontuário: {CodigoProntuario}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Paciente paciente = new Paciente("Débora Pinheiro", 27);
+HistoricoMedico historico = new HistoricoMedico("RJ-3108");
+
+historico.ExibirCodigo();
